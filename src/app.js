3e518d5b32aadcfa53dcbe875751a6e83355e508
veras92 <110496079+veras92@users.noreply.github.com>
2022-08-13 22:37:11 +0300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -42,7 +42,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "8edd95775ec5d6f28bc3fcb0277e2223";
-let city = "Edam";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "8edd95775ec5d6f28bc3fcb0277e2223";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Lisbon");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
